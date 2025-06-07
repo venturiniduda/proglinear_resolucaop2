47547eaba01ae7e3bcd779985908feab7fc63676
Maria Eduarda Venturini Wollmann <mduduv@gmail.com>
2025-06-07 18:46:58 -0300

    Ajustes finais

--- resolucao.py
+++ resolucao.py
@@ -4,8 +4,6 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib.patches import Patch as patches
 
-# Adicionar plot de tabelas, comparando resultados do modelo A e B
-
 ## FORMATAÇÃO DOS RESULTADOS EM TABELAS ##
 def log_solution(instance_name, solution_a, solution_b):
     if solution_a is None or solution_b is None:
@@ -25,10 +23,20 @@ def log_solution(instance_name, solution_a, solution_b):
             routes,
             arrival_times,
             delay_times,
-            *rest  # para capturar o 8º valor se existir
+            *rest
         ) = solution
 
-        max_delay_str = f"\nMaior Tempo de Atraso (max delay): {rest[0]:.2f}" if show_max_delay and rest else ""
+        def fmt(value):
+            return "0.00" if abs(value) == -0.00 else f"{value:.2f}"
+
+        max_delay_str = f"\nMaior Tempo de Atraso (max delay): {fmt(rest[0])}" if show_max_delay and rest else ""
+
+        data = list(zip(routes, arrival_times, delay_times))
+        data_sorted = sorted(data, key=lambda x: x[1])
+
+        # # Junta dados e ordena por horário de chegada
+        data_sorted = list(zip(routes, arrival_times, delay_times))
+        # data_sorted = sorted(data, key=lambda x: x[1])
 
         result = f"""
 Limite superior da funcao objetivo (Atraso Total): {objective_upper_bound:.2f}
@@ -37,8 +45,8 @@ Tempo total de processamento: {runtime}
 Gap Relativo: {relative_gap:.2f}
 Contagem de Nos: {node_count}
 Rotas: {' '.join(map(str, routes))}
-Horarios de Chegada: {' '.join(map(lambda x: f"{x:.2f}", arrival_times))}
-Tempos de Atraso (compacto): {' '.join(map(lambda x: f"{x:.2f}", delay_times))}{max_delay_str}
+Horarios de Chegada: {' '.join(map(fmt, arrival_times))}
+Tempos de Atraso (compacto): {' '.join(map(fmt, delay_times))}{max_delay_str}
 
 +---------------------+----------------+------------------+
 |   RESULTADO REFERENTE AO EXERCICIO 1 - {label:^14}   |
@@ -46,8 +54,8 @@ Tempos de Atraso (compacto): {' '.join(map(lambda x: f"{x:.2f}", delay_times))}{
 | PONTO DA ROTA  | HORA DE CHEGADA    | TEMPO DE ATRASO   |
 +----------------+--------------------+-------------------+
 """
-        for point, arrival, delay in zip(routes, arrival_times, delay_times):
-            result += f"| {str(point):^14} | {arrival:^18.2f} | {delay:^17.2f} |\n"
+        for point, arrival, delay in data_sorted:
+            result += f"| {str(point):^14} | {fmt(arrival):^18} | {fmt(delay):^17} |\n"
         result += "+----------------+--------------------+-------------------+\n"
 
         return result
@@ -77,8 +85,7 @@ def plot_resolucao(instance_name, nodes, route):
     plt.figure(figsize=(8, 6))
     plt.axis('equal')
     plt.margins(0.3)
-    
-    # Definir a cor dos nós: vermelho para o nó inicial (0), azul claro para os demais
+
     node_colors = ['yellow' if i == 0 else 'lightblue' for i in G.nodes]
 
     nx.draw(
@@ -93,7 +100,6 @@ def plot_resolucao(instance_name, nodes, route):
         connectionstyle='arc3,rad=0.1'
     )
 
-     # Adiciona legenda
     legend_elements = [
         patches(facecolor='yellow', edgecolor='black', label='Ponto de Início'),
         patches(facecolor='lightblue', edgecolor='black', label='Demais Pontos')
@@ -103,11 +109,7 @@ def plot_resolucao(instance_name, nodes, route):
     plt.title(instance_name)
     plt.grid(True)
 
-     # Salvar a figura
     filename = f"solucao_{instance_name}.png"
     filepath = os.path.join('./resultados/imagens', filename)
     plt.savefig(filepath, bbox_inches='tight')
-    plt.close()  # fecha a figura para liberar memória
-        
-    
-    
+    plt.close()
